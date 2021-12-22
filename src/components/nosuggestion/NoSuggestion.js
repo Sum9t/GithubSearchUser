d3adec8d38917b0a043e7a8ed6694687429245fe
Sum9t <sumitsinghwork123@gmail.com>
2021-12-22 18:11:33 +0530

    Added no suggestion component

---
+++ src/components/nosuggestion/NoSuggestion.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./style.css";
+export const NoSuggestion = () => {
+	return (
+		<div className='no-suggestions'>
+			<p className='suggestiontext'>No suggestions, you're on your own!</p>
+		</div>
+	);
+};
